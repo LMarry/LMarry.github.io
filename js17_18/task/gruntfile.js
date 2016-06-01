ab4e45b4959404d7847bedcf4c3ac36ec75adf8a
Marry <marry.crate@gmail.com>
2016-06-01 21:17:59 +0300

    js19_20 task1

---
+++ js17_18/task/gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: ';'
+            }
+            , dist: {
+                src: ['js/src/*.js'],
+                dest: 'js/dist/script.main.js'
+            }
+        }
+        , uglify: {
+            dist: {
+                src: ['js/dist/script.main.js'],
+                dest: 'js/dist/script.main.min.js'
+            }
+        },
+        csso: {
+          compress: {
+            options: {
+              report: 'gzip'
+            },
+            files: {
+              'css/dist/style.main.min.css': ['css/*.css']
+            }
+          }
+        }
+        , watch: {
+            concat: {
+                files: '<%= concat.dist.src %>',
+                tasks: 'concat' 
+            },
+            uglify: {
+                files: '<%= uglify.dist.src %>',
+                tasks: 'uglify'
+            }
+        },
+        sass: {
+            dist: {
+              files: [{
+                expand: true,
+                cwd: 'styles',
+                src: ['*.scss'],
+                dest: 'styles',
+                ext: 'main.css'
+              }]
+            }
+          }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-csso');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['concat', 'uglify', 'csso', 'sass']);
+
+};
---
+++ js19_20/task1/js/animation.js
@@ -0,0 +1,62 @@
+$(function () {
+    $('.jcarousel').jcarousel({
+
+    });
+    
+    $('.jcarousel-pagination').jcarouselPagination({
+        item: function (page) {
+            return '<a href="#' + page + '">' + page + '</a>';
+        }
+    });
+    
+    $(document).on('click', '.jcarousel-pagination a', function(){
+        console.log($(this));
+        $.each($('.jcarousel-pagination a'), function(){
+            $(this).css({'background-color': '#ffffff'});
+        });
+        $(this).css({'background-color': '#000000'});
+    });
+    
+    
+//    $('select').styler();
+    
+    $( '.dropdown' ).hover(
+    function(){
+        $(this).children('.sub-menu').slideDown(200)
+        .animate({
+        backgroundColor:'#fc9f9f'
+        }, 500 );
+        
+
+    },
+    function(){
+        $(this).children('.sub-menu').slideUp(200)
+        .animate({
+        backgroundColor:'#e00'
+        }, 500 );
+    })
+    
+    $(".banner__info").hide();
+	$(".banner__expand").click(function(){
+        var $banner_hover = $(".banner_hover");
+        var $banner__expand = $(this);
+        var $banner = $(this).parent().parent();
+        
+        if ($banner_hover.get(0) == $banner.get(0)) {
+
+            $banner.removeClass('banner_hover');
+            $banner__expand.html('+');
+            $banner__expand.parent().next().slideToggle();
+        }   else {
+            $banner_hover.removeClass('banner_hover');
+            $banner_hover.find('.banner__expand').html('+');
+            $banner_hover.find('div').slideToggle();
+            $banner.addClass('banner_hover');
+            $banner__expand.html('-');
+            $banner__expand.parent().next().slideToggle();            
+        }  
+	});
+    
+    $(".banner__expand").first().trigger("click");
+});
+
